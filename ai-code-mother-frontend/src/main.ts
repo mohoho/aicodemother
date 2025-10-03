a3fb3b6572ed75bfcf49f6a06ba60ee813ab9031
大黑 <380692614@qq.com>
2025-10-03 13:06:58 +0800

    feat:前端初始化

--- ai-code-mother-frontend/src/main.ts
+++ ai-code-mother-frontend/src/main.ts
@@ -1,7 +1,8 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-app.mount('#app')
+app.use(Antd)
+app.mount('#app');
