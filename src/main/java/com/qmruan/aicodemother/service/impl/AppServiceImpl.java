e840d9c0ac2999f1ca36a6695950962d4aa14baa
qmruan <qmruan@trip.com>
2026-01-28 14:11:19 +0800

    feat: 部署准备

--- src/main/java/com/qmruan/aicodemother/service/impl/AppServiceImpl.java
+++ src/main/java/com/qmruan/aicodemother/service/impl/AppServiceImpl.java
@@ -30,6 +30,7 @@ import com.qmruan.aicodemother.service.ScreenshotService;
 import com.qmruan.aicodemother.service.UserService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -71,6 +72,9 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
 
     @Resource
     private AiCodeGenTypeRoutingService aiCodeGenTypeRoutingService;
+
+    @Value("${code.deploy-host:http://localhost}")
+    private String deployHost;
 
     @Override
     public Long createApp(AppAddRequest appAddRequest, User loginUser) {
@@ -257,7 +261,7 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppS
         ThrowUtils.throwIf(!res, ErrorCode.OPERATION_ERROR, "更新应用部署信息失败");
 
         // 10. 构建应用访问 URL
-        String appDeployUrl = String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);
+        String appDeployUrl = String.format("%s/%s/", deployHost, deployKey);
 // 11. 异步生成截图并更新应用封面
         generateAppScreenshotAsync(appId, appDeployUrl);
         return appDeployUrl;
